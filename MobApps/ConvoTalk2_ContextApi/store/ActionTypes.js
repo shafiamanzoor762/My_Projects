e197d78ed9ef84342ebdec19bd46cd878ae61ca5
Shafoo <100573997+shafiamanzoor762@users.noreply.github.com>
2024-05-02 09:37:22 -0700

    Delete MobApps directory

--- MobApps/ConvoTalk2_ContextApi/store/ActionTypes.js
+++
@@ -1,8 +0,0 @@
-export const INCREASE='INCREASE'
-export const DECREASE='DECREASE'
-
-export const ADD_VALUE='ADD_VALUE'
-export const REMOVE_VALUE='REMOVE_VALUE'
-
-export const ADD_USER='ADD_USER'
-export const DEL_USER='DEL_USER'
